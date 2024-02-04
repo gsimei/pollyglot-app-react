a1d2bd14a2c5a5d75013228c16a862a05107be4e
George Simei <georgesimei@gmail.com>
2024-02-04 17:40:36 +0100

    Uploaded new images and created a new folder for them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
-import './App.css';
+import './App.css'; 
 
 function App() {
   const [inputText, setInputText] = useState('');
   const [selectedLanguage, setSelectedLanguage] = useState('French');
   const [translatedTexts, setTranslatedTexts] = useState([]);
+
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [translatedTexts]);
 
   const handleTranslate = async () => {
     const data = { translation: { text: inputText, language: selectedLanguage } };
@@ -34,50 +44,67 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Translator App</h1>
+      <div className="hero-section">
+        <img src="/vector.png" alt="PollyGlot AI" className="vector-img" />
+        <div className="logo">
+          <img src="/parrot.png" alt="PollyGlot AI" className="parrot-img" />
+        </div>
+        <div className="txt-content">
+          <h1>PollyGlot AI</h1>
+          <p>Perfect Translation Every Time</p>
+        </div>
+      </div>
 
-      <div>
-        {translatedTexts.map((text, index) => (
-          <div key={index} className="message">
-            <p className="input">{text.input}</p>
-            <p className="translation">{text.translation}</p>
+      <div className="container">
+        <div className="chat-container">
+          <div className="messages">
+          <p className="translation">Select the language you want me to translate into, type your text, and hit send! </p>
+            {translatedTexts.map((text, index) => (
+              <div key={index} className="message">
+                <p className="input">{text.input}</p>
+                <p className="translation">{text.translation}</p>
+              </div>
+            ))}
+            <div ref={messagesEndRef} /> {/* Invisible element at the end of messages */}
           </div>
-        ))}
+        </div>
+        <div className="input-container">
+          <div className="input-icon">
+            <FontAwesomeIcon icon={faPaperPlane} onClick={handleTranslate} className='icon' />
+            <input
+              type="text"
+              value={inputText}
+              onChange={(e) => setInputText(e.target.value)}
+            />
+          </div>
+          <div className='flags'>
+            <img
+              src="/flag-french.png"
+              alt="French flag"
+              className={`french-flag ${selectedLanguage === 'French' ? 'flag-selected' : ''}`}
+              onClick={() => setSelectedLanguage('French')}
+            />
+            <img
+              src="/flag-spanish.png"
+              alt="Spanish flag"
+              className={`spanish-flag ${selectedLanguage === 'Spanish' ? 'flag-selected' : ''}`}
+              onClick={() => setSelectedLanguage('Spanish')}
+            />
+            <img
+              src="/flag-germany.png"
+              alt="Germany flag"
+              className={`spanish-flag ${selectedLanguage === 'Germany' ? 'flag-selected' : ''}`}
+              onClick={() => setSelectedLanguage('Germany')}
+            />
+            <img
+              src="/flag-japan.png"
+              alt="Japonese flag"
+              className={`japan-flag ${selectedLanguage === 'Japonese' ? 'flag-selected' : ''}`}
+              onClick={() => setSelectedLanguage('Japonese')}
+            />
+          </div>
+        </div>
       </div>
-      <div className="input-icon">
-        <FontAwesomeIcon icon={faPaperPlane} onClick={handleTranslate} className='icon' />
-        <input
-          type="text"
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-        />
-      </div>
-      <div className='flags'>
-      <img
-        src="/flag-french.png"
-        alt="French flag"
-        className={`french-flag ${selectedLanguage === 'French' ? 'flag-selected' : ''}`}
-        onClick={() => setSelectedLanguage('French')}
-      />
-      <img
-        src="/flag-spanish.png"
-        alt="Spanish flag"
-        className={`spanish-flag ${selectedLanguage === 'Spanish' ? 'flag-selected' : ''}`}
-        onClick={() => setSelectedLanguage('Spanish')}
-      />
-      <img
-        src="/flag-germany.png"
-        alt="German flag"
-        className={`germany-flag ${selectedLanguage === 'Germany' ? 'flag-selected' : ''}`}
-        onClick={() => setSelectedLanguage('German')}
-      />
-      <img
-        src="/flag-japan.png"
-        alt="German flag"
-        className={`japan-flag ${selectedLanguage === 'Japonese' ? 'flag-selected' : ''}`}
-        onClick={() => setSelectedLanguage('Japonese')}
-      />
-    </div>
     </div>
   );
 }
